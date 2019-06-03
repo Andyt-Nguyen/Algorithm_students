5c55c039e12976a958097789a9e39f23c1d94a95
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2019-06-02 19:13:33 -0700

    problem set 2.1 no test has been created

---
+++ problem_set_2/index.js
@@ -0,0 +1,19 @@
+/* 
+
+    Problem 2.1
+    Prompt: Create a function called countChars that counts the characters in a string and returns the object of the counted charcters
+    
+    Test Cases:
+        (This only test some cases for you to get an idea. This does not reflect if your algo works for all test cases)
+        (run `npm run test` to check if your alg passes the tests.)
+
+        "aabbcc" return { a: 2, b: 2, c: 2 }
+        "HiMrCrabs" return { H: 1, M: 1, r:2, C: 1, a: 1, b: 1, s: 1 }
+        "mississippi" return { m: 1, i: 4, s: 4, p: 2}
+
+*/ 
+
+
+function countChars(string){
+    return ;
+ }
